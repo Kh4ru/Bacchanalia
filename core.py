15d504e730911ff9f48988252c2cb310a9010a82
Alexis <smiley.roadofhapiness@gmail.com>
2026-01-24 23:57:00 +0100

    Update 1.5

--- core.py
+++ core.py
@@ -1,5 +1,5 @@
-import sqlite3,os
-from flask import Flask,request,send_from_directory,render_template,url_for
+import sqlite3,os,bcrypt
+from flask import Flask,request,send_from_directory,render_template,url_for,session,redirect
 songs_url = os.path.join(os.path.dirname(__file__), 'audios')
 covers_url = os.path.join(os.path.dirname(__file__), 'static/covers')
 
@@ -39,9 +39,12 @@ def search(text):
     return result
 
 app = Flask(__name__)
+app.secret_key = "itsasecretlol"
 
 @app.route("/")
 def hello_world():
+    if "user_id" not in session:
+        return redirect("/login")
     q = request.args.get('q')
     songs = search(q) if q else all_songs()
     length = list_size()
@@ -51,7 +54,57 @@ def hello_world():
         file = f"{r[4]}.mp3"
         song = Song(r[1],r[2],r[3],file,cover)
         results.append(song)
-    return render_template("web_client.html",songs=results,q=q,length=length)
+    return render_template("index.html",songs=results,q=q,length=length)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"]
+        db = sqlite3.connect("songs.db")
+        cur = db.cursor()
+        cur.execute("SELECT id, password, name FROM users WHERE userid = ?", (username,))
+        row = cur.fetchone()
+
+        if row:
+            user_id,password_hash,name = row
+            if isinstance(password_hash, str):
+                password_hash = password_hash.encode("utf-8")
+
+
+            if bcrypt.checkpw(password.encode("utf-8"), password_hash):
+                session["user_id"] = user_id
+                session["name"] = name
+                return redirect("/")
+
+        return render_template("login.html", error="Identifiants incorrects")
+
+    return render_template("login.html")
+
+
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"].strip()
+        username = request.form["username"].strip()
+        password = request.form["password"]
+        password = password.encode("utf-8")
+        password_hash = bcrypt.hashpw(password,bcrypt.gensalt())
+        db = sqlite3.connect("songs.db")
+        cur = db.cursor()
+        cur.execute(
+    "INSERT INTO users (userid, password, name) VALUES (?, ?, ?)",
+    (username, password_hash, name))
+        db.commit()
+        db.close()
+        return render_template("account_created.html")
+    return render_template("register.html")
+
+@app.route('/logout',methods=["POST"])
+def logout():
+    session.clear()
+    return redirect("/")
 
 @app.route('/play/<filename>')
 def get_audio(filename):
